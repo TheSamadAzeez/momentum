c88ce7ec423dd4b18773adf019674cb68555c8d4
TheSamadAzeez <azeezsamad2004@gmail.com>
2026-01-10 11:19:53 +0100

    feat: configure global settings and initialize database and users modules

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,29 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule, ConfigService } from '@nestjs/config';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { UsersModule } from './users/users.module';
+import { DatabaseModule } from './database/database.module';
+import databaseConfig from './database/database.config';
 
 @Module({
-  imports: [],
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+      load: [databaseConfig],
+    }),
+    DatabaseModule.forRootAsync({
+      inject: [ConfigService],
+      useFactory: (configService: ConfigService) => ({
+        host: configService.get('database.host')!,
+        port: configService.get('database.port')!,
+        user: configService.get('database.user')!,
+        password: configService.get('database.password')!,
+        database: configService.get('database.database')!,
+      }),
+    }),
+    UsersModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
