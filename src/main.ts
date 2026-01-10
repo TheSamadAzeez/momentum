e1d6ee7460f97fafe664ff66d383b34856ac3a29
TheSamadAzeez <azeezsamad2004@gmail.com>
2026-01-10 22:05:47 +0100

    feat: add cookie-session middleware for session management

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,17 @@
 import { NestFactory } from '@nestjs/core';
 import { ValidationPipe } from '@nestjs/common';
 import { AppModule } from './app.module';
+import cookieSession from 'cookie-session';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.use(
+    cookieSession({
+      keys: ['momentum-secret-key'], // TODO: use environment variable
+      // secure: process.env.NODE_ENV === 'production',
+      maxAge: 24 * 60 * 60 * 1000, // 1 day TODO: change to 7 days
+    }),
+  );
   app.useGlobalPipes(
     new ValidationPipe({
       whitelist: true,
