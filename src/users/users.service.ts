18a5f464d79ee4df01332b6cc3450cb5ab47211b
TheSamadAzeez <azeezsamad2004@gmail.com>
2026-01-10 14:19:13 +0100

    feat: implement user creation and update find/delete methods to use string IDs

--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -1,23 +1,39 @@
 import { Injectable } from '@nestjs/common';
+import { eq } from 'drizzle-orm';
 import { DrizzleService } from 'src/database/drizzle.service';
+import { usersTable } from 'src/database/schemas/users';
 
 @Injectable()
 export class UsersService {
   constructor(private readonly drizzleService: DrizzleService) {}
 
-  findOne(id: number) {
-    console.log(`user id: ${id}`);
+  create(email: string, password: string, firstName: string, lastName: string) {
+    return this.drizzleService.db
+      .insert(usersTable)
+      .values({ email, password, firstName, lastName });
+  }
+
+  findOne(id: string) {
+    return this.drizzleService.db
+      .select()
+      .from(usersTable)
+      .where(eq(usersTable.id, id));
   }
 
   findOneByEmail(email: string) {
-    console.log(`user email: ${email}`);
+    return this.drizzleService.db
+      .select()
+      .from(usersTable)
+      .where(eq(usersTable.email, email));
   }
 
   findAll() {
-    console.log('all users');
+    return this.drizzleService.db.select().from(usersTable);
   }
 
-  delete(id: number) {
-    console.log(`deleted user with id: ${id}`);
+  delete(id: string) {
+    return this.drizzleService.db
+      .delete(usersTable)
+      .where(eq(usersTable.id, id));
   }
 }
