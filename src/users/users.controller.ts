1793b2b33180b6a915905d78afc248e1e3923fa7
TheSamadAzeez <azeezsamad2004@gmail.com>
2026-01-10 14:19:57 +0100

    feat: implement signup and signin methods in UsersController

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -1,4 +1,23 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Post, Body } from '@nestjs/common';
+import { AuthService } from './auth.service';
+import { CreateUserDto } from './dtos/create-user.dto';
 
 @Controller('users')
-export class UsersController {}
+export class UsersController {
+  constructor(private authService: AuthService) {}
+
+  @Post('auth/signup')
+  signup(@Body() body: CreateUserDto) {
+    return this.authService.signup(
+      body.email,
+      body.password,
+      body.firstName,
+      body.lastName,
+    );
+  }
+
+  @Post('auth/signin')
+  signin(@Body() body: CreateUserDto) {
+    return this.authService.signin(body.email, body.password);
+  }
+}
