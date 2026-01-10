1ba5386cf1024503ddc1aed3c3d775edba221cb4
TheSamadAzeez <azeezsamad2004@gmail.com>
2026-01-10 22:07:33 +0100

    refactor: update signup and signin methods to use session management

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -1,23 +1,32 @@
-import { Controller, Post, Body } from '@nestjs/common';
+import { Controller, Post, Body, Session } from '@nestjs/common';
 import { AuthService } from './auth.service';
 import { CreateUserDto } from './dtos/create-user.dto';
+
+interface SessionData {
+  userId?: number | string;
+}
 
 @Controller('users')
 export class UsersController {
   constructor(private authService: AuthService) {}
 
   @Post('auth/signup')
-  signup(@Body() body: CreateUserDto) {
-    return this.authService.signup(
-      body.email,
-      body.password,
-      body.firstName,
-      body.lastName,
-    );
+  async signup(@Body() body: CreateUserDto, @Session() session: SessionData) {
+    const user = await this.authService.signup(body.email, body.password);
+    session.userId = user?.id;
+    return user;
   }
 
   @Post('auth/signin')
-  signin(@Body() body: CreateUserDto) {
-    return this.authService.signin(body.email, body.password);
+  async signin(@Body() body: CreateUserDto, @Session() session: SessionData) {
+    const user = await this.authService.signin(body.email, body.password);
+    session.userId = user?.id;
+    return user;
+  }
+
+  @Post('auth/signout')
+  signout(@Session() session: SessionData) {
+    this.authService.signout(session);
+    return { message: 'User signed out successfully' };
   }
 }
