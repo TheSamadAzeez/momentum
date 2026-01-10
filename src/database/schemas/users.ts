8f3efbb28362308e2d6ed9d0225ba2392a7ef0e1
TheSamadAzeez <azeezsamad2004@gmail.com>
2026-01-10 14:18:27 +0100

    feat: create users table schema in database

---
+++ src/database/schemas/users.ts
@@ -0,0 +1,11 @@
+import { pgTable, text, timestamp, uuid } from 'drizzle-orm/pg-core';
+
+export const usersTable = pgTable('users', {
+  id: uuid('id').primaryKey().defaultRandom(),
+  email: text('email').notNull().unique(),
+  password: text('password').notNull(),
+  firstName: text('first_name').notNull(),
+  lastName: text('last_name').notNull(),
+  createdAt: timestamp('created_at').defaultNow().notNull(),
+  updatedAt: timestamp('updated_at').defaultNow().notNull(),
+});
