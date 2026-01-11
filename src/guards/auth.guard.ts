214b9d98fa7198144eb550749f8b9711a5bc5195
TheSamadAzeez <azeezsamad2004@gmail.com>
2026-01-11 21:27:47 +0100

    feat: implement AuthGuard to manage user authentication

---
+++ src/guards/auth.guard.ts
@@ -0,0 +1,23 @@
+import {
+  CanActivate,
+  ExecutionContext,
+  Injectable,
+  UnauthorizedException,
+} from '@nestjs/common';
+import { Observable } from 'rxjs';
+import { Request } from 'express';
+
+// guard to check if user is authenticated
+@Injectable()
+export class AuthGuard implements CanActivate {
+  canActivate(
+    context: ExecutionContext,
+  ): boolean | Promise<boolean> | Observable<boolean> {
+    const request = context.switchToHttp().getRequest<Request>();
+
+    if (!request.user) {
+      throw new UnauthorizedException('User is not authenticated');
+    }
+    return true;
+  }
+}
