2ceab59d69836508515b77cd44ff312134a25fb8
TheSamadAzeez <azeezsamad2004@gmail.com>
2026-01-11 21:27:13 +0100

    feat: export UsersService from UsersModule

--- src/users/users.module.ts
+++ src/users/users.module.ts
@@ -6,5 +6,6 @@ import { AuthService } from './auth.service';
 @Module({
   controllers: [UsersController],
   providers: [UsersService, AuthService],
+  exports: [UsersService],
 })
 export class UsersModule {}
