4f385d038c38117ac483f45519b0e75e78665661
TheSamadAzeez <azeezsamad2004@gmail.com>
2026-01-12 09:52:06 +0100

    feat: add habitsTable to databaseSchema export

--- src/database/schemas/index.ts
+++ src/database/schemas/index.ts
@@ -1,7 +1,9 @@
 import { usersTable } from './users';
+import { habitsTable } from './habits';
 
 export const databaseSchema = {
   usersTable,
+  habitsTable,
 };
 
 // export type createUserType = typeof usersTable.$inferInsert;
