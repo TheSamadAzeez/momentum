e748a57ce646694a768bdeb05011889c924cb709
TheSamadAzeez <azeezsamad2004@gmail.com>
2026-01-12 12:29:16 +0100

    feat: add habit completion and logging functionality to HabitsService

--- src/habits/habits.service.ts
+++ src/habits/habits.service.ts
@@ -2,6 +2,7 @@ import { Injectable, NotFoundException } from '@nestjs/common';
 import { DrizzleService } from 'src/database/drizzle.service';
 import { and, eq } from 'drizzle-orm';
 import { habitsTable } from 'src/database/schemas/habits';
+import { habitLogTable } from 'src/database/schemas/habit-log';
 import { createHabitDTO } from './dtos/create-habit.dto';
 
 @Injectable()
@@ -24,9 +25,9 @@ export class HabitsService {
       .where(eq(habitsTable.userId, userId));
   }
 
-  async getHabitById(id: string, userId: string) {
+  async getHabitById(habitId: string, userId: string) {
     const habit = await this.drizzleService.db.query.habitsTable.findFirst({
-      where: and(eq(habitsTable.id, id), eq(habitsTable.userId, userId)),
+      where: and(eq(habitsTable.id, habitId), eq(habitsTable.userId, userId)),
     });
 
     if (!habit) {
@@ -36,20 +37,54 @@ export class HabitsService {
     return habit;
   }
 
-  async updateHabit(id: string, userId: string, habitData: createHabitDTO) {
+  async updateHabit(
+    habitId: string,
+    userId: string,
+    habitData: createHabitDTO,
+  ) {
     await this.drizzleService.db
       .update(habitsTable)
       .set(habitData)
-      .where(and(eq(habitsTable.id, id), eq(habitsTable.userId, userId)));
+      .where(and(eq(habitsTable.id, habitId), eq(habitsTable.userId, userId)));
 
     return { message: 'Habit updated successfully' };
   }
 
-  async deleteHabit(id: string, userId: string) {
+  async deleteHabit(habitId: string, userId: string) {
     await this.drizzleService.db
       .delete(habitsTable)
-      .where(and(eq(habitsTable.id, id), eq(habitsTable.userId, userId)));
+      .where(and(eq(habitsTable.id, habitId), eq(habitsTable.userId, userId)));
 
     return { message: 'Habit deleted successfully' };
+  }
+
+  async completeHabit(habitId: string, userId: string) {
+    // Verify the habit exists and belongs to the user
+    const habit = await this.getHabitById(habitId, userId);
+
+    if (!habit) {
+      throw new NotFoundException('Habit not found');
+    }
+
+    await this.drizzleService.db.insert(habitLogTable).values({
+      habitId: habit.id,
+      completed: true,
+    });
+
+    // TODO: Update streak and Update longest streak if needed
+    return { message: 'Habit marked as completed' };
+  }
+
+  async getHabitLogs(habitId: string, userId: string) {
+    const habit = await this.getHabitById(habitId, userId);
+
+    if (!habit) {
+      throw new NotFoundException('Habit not found');
+    }
+
+    return await this.drizzleService.db
+      .select()
+      .from(habitLogTable)
+      .where(eq(habitLogTable.habitId, habitId));
   }
 }
