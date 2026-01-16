28b038f7f8351e440f7ee254106b78cfaed6f9f8
TheSamadAzeez <azeezsamad2004@gmail.com>
2026-01-16 20:21:16 +0100

    feat: add StreakSchedulerService to StreakModule providers

--- src/streak/streak.module.ts
+++ src/streak/streak.module.ts
@@ -1,9 +1,10 @@
 import { Module } from '@nestjs/common';
 import { StreakController } from './streak.controller';
 import { StreakService } from './streak.service';
+import { StreakSchedulerService } from './streak-scheduler.service';
 
 @Module({
   controllers: [StreakController],
-  providers: [StreakService]
+  providers: [StreakService, StreakSchedulerService],
 })
 export class StreakModule {}
