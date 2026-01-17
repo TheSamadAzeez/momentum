6ec72a17d14fd34d8008f40c8065899209097aa6
TheSamadAzeez <azeezsamad2004@gmail.com>
2026-01-17 13:19:18 +0100

    feat: update module imports and exports for Streak and Users modules

--- src/streak/streak.module.ts
+++ src/streak/streak.module.ts
@@ -2,9 +2,12 @@ import { Module } from '@nestjs/common';
 import { StreakController } from './streak.controller';
 import { StreakService } from './streak.service';
 import { StreakSchedulerService } from './streak-scheduler.service';
+import { UsersModule } from 'src/users/users.module';
 
 @Module({
+  imports: [UsersModule],
   controllers: [StreakController],
   providers: [StreakService, StreakSchedulerService],
+  exports: [StreakService],
 })
 export class StreakModule {}
