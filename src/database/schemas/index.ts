07113e3c08c3b049743075976e26953af9f928c2
TheSamadAzeez <azeezsamad2004@gmail.com>
2026-01-30 19:13:09 +0100

    feat: add notificationsTable to database schema

--- src/database/schemas/index.ts
+++ src/database/schemas/index.ts
@@ -2,12 +2,14 @@ import { usersTable } from './users';
 import { habitsTable } from './habits';
 import { habitLogTable } from './habit-log';
 import { streaksTable } from './streaks';
+import { notificationsTable } from './notifications';
 
 export const databaseSchema = {
   usersTable,
   habitsTable,
   habitLogTable,
   streaksTable,
+  notificationsTable,
 };
 
 // export type createUserType = typeof usersTable.$inferInsert;
