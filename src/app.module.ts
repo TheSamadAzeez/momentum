ece5d0cd4e02731ae2efac705beeb4129b517fd7
TheSamadAzeez <azeezsamad2004@gmail.com>
2026-01-30 19:09:28 +0100

    feat: integrate BullMQ for job processing and add NotificationModule

--- src/app.module.ts
+++ src/app.module.ts
@@ -10,10 +10,25 @@ import { UserSessionMiddleware } from './middleware/user-session.middleware';
 import { HabitsModule } from './habits/habits.module';
 import { AnalyticsModule } from './analytics/analytics.module';
 import { StreakModule } from './streak/streak.module';
+import { NotificationModule } from './notification/notification.module';
+// import { CacheModule } from '@nestjs/cache-manager';
+import { BullModule } from '@nestjs/bullmq';
 
 @Module({
   imports: [
     ScheduleModule.forRoot(),
+    BullModule.forRoot({
+      connection: {
+        host: 'localhost',
+        port: 6379,
+      },
+      // defaultJobOptions: { attempts: 3 },
+    }),
+    // CacheModule.register({
+    //   max: 100, // max number of items in cache i.e. 100 habits can be stored in cache at a time
+    //   isGlobal: true,
+    //   ttl: 60 * 60 * 1000, // 1 hour
+    // }),
     ConfigModule.forRoot({
       isGlobal: true,
       load: [databaseConfig],
@@ -32,6 +47,7 @@ import { StreakModule } from './streak/streak.module';
     HabitsModule,
     AnalyticsModule,
     StreakModule,
+    NotificationModule,
   ],
   controllers: [AppController],
   providers: [AppService],
